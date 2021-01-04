59f3eee548ea5dd245f2f78967c49739baedda03
Somesh <somesh.bhaskar@webdunia.net>
2021-01-05 00:15:36 +0530

    added breed json

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import torch
 import torchvision.models as models
 import torch.nn as nn
 from flask_cors import CORS
-
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -24,165 +24,69 @@ model.fc = nn.Linear(2048, num_classes, bias = True)
 model.load_state_dict(torch.load('classifier.pt', map_location=torch.device('cpu')))
 model.eval()
 
-class_names = ['Affenpinscher',
- 'Afghan hound',
- 'Airedale terrier',
- 'Akita',
- 'Alaskan malamute',
- 'American eskimo dog',
- 'American foxhound',
- 'American staffordshire terrier',
- 'American water spaniel',
- 'Anatolian shepherd dog',
- 'Australian cattle dog',
- 'Australian shepherd',
- 'Australian terrier',
- 'Basenji',
- 'Basset hound',
- 'Beagle',
- 'Bearded collie',
- 'Beauceron',
- 'Bedlington terrier',
- 'Belgian malinois',
- 'Belgian sheepdog',
- 'Belgian tervuren',
- 'Bernese mountain dog',
- 'Bichon frise',
- 'Black and tan coonhound',
- 'Black russian terrier',
- 'Bloodhound',
- 'Bluetick coonhound',
- 'Border collie',
- 'Border terrier',
- 'Borzoi',
- 'Boston terrier',
- 'Bouvier des flandres',
- 'Boxer',
- 'Boykin spaniel',
- 'Briard',
- 'Brittany',
- 'Brussels griffon',
- 'Bull terrier',
- 'Bulldog',
- 'Bullmastiff',
- 'Cairn terrier',
- 'Canaan dog',
- 'Cane corso',
- 'Cardigan welsh corgi',
- 'Cavalier king charles spaniel',
- 'Chesapeake bay retriever',
- 'Chihuahua',
- 'Chinese crested',
- 'Chinese shar-pei',
- 'Chow chow',
- 'Clumber spaniel',
- 'Cocker spaniel',
- 'Collie',
- 'Curly-coated retriever',
- 'Dachshund',
- 'Dalmatian',
- 'Dandie dinmont terrier',
- 'Doberman pinscher',
- 'Dogue de bordeaux',
- 'English cocker spaniel',
- 'English setter',
- 'English springer spaniel',
- 'English toy spaniel',
- 'Entlebucher mountain dog',
- 'Field spaniel',
- 'Finnish spitz',
- 'Flat-coated retriever',
- 'French bulldog',
- 'German pinscher',
- 'German shepherd dog',
- 'German shorthaired pointer',
- 'German wirehaired pointer',
- 'Giant schnauzer',
- 'Glen of imaal terrier',
- 'Golden retriever',
- 'Gordon setter',
- 'Great dane',
- 'Great pyrenees',
- 'Greater swiss mountain dog',
- 'Greyhound',
- 'Havanese',
- 'Ibizan hound',
- 'Icelandic sheepdog',
- 'Irish red and white setter',
- 'Irish setter',
- 'Irish terrier',
- 'Irish water spaniel',
- 'Irish wolfhound',
- 'Italian greyhound',
- 'Japanese chin',
- 'Keeshond',
- 'Kerry blue terrier',
- 'Komondor',
- 'Kuvasz',
- 'Labrador retriever',
- 'Lakeland terrier',
- 'Leonberger',
- 'Lhasa apso',
- 'Lowchen',
- 'Maltese',
- 'Manchester terrier',
- 'Mastiff',
- 'Miniature schnauzer',
- 'Neapolitan mastiff',
- 'Newfoundland',
- 'Norfolk terrier',
- 'Norwegian buhund',
- 'Norwegian elkhound',
- 'Norwegian lundehund',
- 'Norwich terrier',
- 'Nova scotia duck tolling retriever',
- 'Old english sheepdog',
- 'Otterhound',
- 'Papillon',
- 'Parson russell terrier',
- 'Pekingese',
- 'Pembroke welsh corgi',
- 'Petit basset griffon vendeen',
- 'Pharaoh hound',
- 'Plott',
- 'Pointer',
- 'Pomeranian',
- 'Poodle',
- 'Portuguese water dog',
- 'Saint bernard',
- 'Silky terrier',
- 'Smooth fox terrier',
- 'Tibetan mastiff',
- 'Welsh springer spaniel',
- 'Wirehaired pointing griffon',
- 'Xoloitzcuintli',
- 'Yorkshire terrier']
 
+def resnet50_predict(pil_image):
+    '''
+    Use pre-trained VGG-16 model to obtain index corresponding to 
+    predicted ImageNet class for image at specified path
+    
+    Args:
+        img_path: path to an image
+        
+    Returns:
+        Index corresponding to VGG-16 model's prediction
+    '''
+    
+    ## TODO: Complete the function.
+    ## Load and pre-process an image from the given img_path
+    ## Return the *index* of the predicted class for that image
+    # Image Resize to 256
+    resnet50 = models.resnet50(pretrained=True)
+    mean=[0.485, 0.456, 0.406]
+    std=[0.229, 0.224, 0.225]
+    image_transforms = transforms.Compose([transforms.Resize(256),
+                                           transforms.CenterCrop(224),
+                                           transforms.ToTensor(),
+                                           transforms.Normalize(mean,std)])
+    image_tensor = image_transforms(pil_image)
+    image_tensor.unsqueeze_(0)
+    resnet50.eval()
+    output = resnet50(image_tensor)
+    _,classes= torch.max(output,dim=1)
+    return classes.item() # predicted class index
 
-def load_image(image_bytes):
+def dog_detector(pil_image):
+    ## TODO: Complete the function.
+    class_dog=resnet50_predict(pil_image)
+    return class_dog >= 151 and class_dog <=268 # true/false
+
+def transform_image(image):
     transform = transforms.Compose([transforms.Resize((224, 224)),
                                     transforms.ToTensor(),
                                     transforms.Normalize((0.5, 0.5, 0.5), (0.25, 0.25, 0.25))])
                             
-    image = Image.open(io.BytesIO(image_bytes))
     return transform(image)[:3,:,:].unsqueeze(0)
 
-
-def predict_breed_transfer(image_bytes):
+def predict_breed_transfer(image):
     # load the image and return the predicted breed
-    image = load_image(image_bytes)
-    idx = torch.argmax(model(image))
-    print(class_names[idx])
-    return class_names[idx]
+    transformed_image = transform_image(image)
+    idx = torch.argmax(model(transformed_image))
+    with open("dog_breeds.json") as f:
+        class_names = json.loads(f.read())
+        return class_names[idx]
     
 @app.route('/', methods=['POST'])
 def predict():
     if request.method == 'POST':
         file = request.files['file']
-        print(file)
-        img_bytes = file.read()
-        class_name = predict_breed_transfer(image_bytes=img_bytes)
-        return jsonify({'class_name': class_name})
+        image_bytes = file.read()
+        image = Image.open(io.BytesIO(image_bytes))
+
+        if (dog_detector(image)):
+            class_name = predict_breed_transfer(image)
+            return jsonify({'class_name': class_name})
+        else:
+            return jsonify({'class_name': "Not a dog image"})
 
 @app.route('/ping', methods=['GET'])
 def ping():
